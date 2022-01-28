8df04e03a8bb5382a7d618dda663032a31e8b253
Hailin-Wang <2011133@tongji.edu.cn>
2022-01-28 14:53:56 +0800

    Update AbaqusExecutor.py

--- abaqus/Tools/AbaqusExecutor/AbaqusExecutor.py
+++ abaqus/Tools/AbaqusExecutor/AbaqusExecutor.py
@@ -1,6 +1,5 @@
 import os
 
-import matplotlib.pyplot as plt
 from PyQt5.QtWidgets import QMessageBox
 
 from .AbaqusExecutorView import AbaqusExecutorView
@@ -23,10 +22,12 @@ class AbaqusExecutor(AbaqusExecutorView):
                     activeWindow.ui.input.setText(file)
                     activeWindow.model.input = file
                     break
-
-        self.process.setWorkingDirectory(os.path.abspath(activeWindow.model.workDirectory))
-        self.process.start(*activeWindow.model.createModelCommand())
-        self.process.finished.connect(setInput)
+        try:
+            self.process.setWorkingDirectory(os.path.abspath(activeWindow.model.workDirectory))
+            self.process.start(*activeWindow.model.createModelCommand())
+            self.process.finished.connect(setInput)
+        except Exception as e:
+            QMessageBox.warning(self, 'Warning', repr(e))
 
     def submit(self):
         activeWindow = self.currentSubWidget()
@@ -40,21 +41,27 @@ class AbaqusExecutor(AbaqusExecutorView):
                     activeWindow.ui.odb.setText(file)
                     activeWindow.model.odb = file
                     break
+                
+        try:
+            self.thread.setWorkDirectory(os.path.abspath(activeWindow.model.workDirectory))
+            self.thread.setJobName(activeWindow.model.jobName())
+            self.thread.start()
 
-        self.thread.setWorkDirectory(os.path.abspath(activeWindow.model.workDirectory))
-        self.thread.setJobName(activeWindow.model.jobName())
-        self.thread.start()
-
-        self.process.setWorkingDirectory(os.path.abspath(activeWindow.model.workDirectory))
-        self.process.start(*activeWindow.model.abaqusCommand())
-        self.process.finished.connect(setOdb)
+            self.process.setWorkingDirectory(os.path.abspath(activeWindow.model.workDirectory))
+            self.process.start(*activeWindow.model.abaqusCommand())
+            self.process.finished.connect(setOdb)
+        except Exception as e:
+            QMessageBox.warning(self, 'Warning', repr(e))
 
     def extractOutput(self):
         activeWindow = self.currentSubWidget()
         if activeWindow is None:
             return
-        self.process.setWorkingDirectory(os.path.abspath(activeWindow.model.workDirectory))
-        self.process.start(*activeWindow.model.extractOutputCommand())
+        try:
+            self.process.setWorkingDirectory(os.path.abspath(activeWindow.model.workDirectory))
+            self.process.start(*activeWindow.model.extractOutputCommand())
+        except Exception as e:
+            QMessageBox.warning(self, 'Warning', repr(e))
 
     def drawFigures(self):
         activeWindow = self.currentSubWidget()
