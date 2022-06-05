4505ff319495ea70260e123471f51ba98cd5b940
Hailin Wang <2011133@tongji.edu.cn>
2022-06-05 11:14:28 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Type hints for Abaqus/Python scripting",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/haiiliin/.",
+    url="https://github.com/haiiliin/pyabaqus",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
