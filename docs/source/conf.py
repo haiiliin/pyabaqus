eb1a58ba131e4b8dca32892ea20bffd7e3fc6b5f
Hailin-Wang <2011133@tongji.edu.cn>
2022-06-15 14:00:47 +0800

    Update to V1.0.19

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, WANG Hailin'
 author = 'WANG Hailin'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.18'
+release = '1.0.19'
 
 
 # -- General configuration ---------------------------------------------------
